203b96c12f35e8cb819e3d396a0623eebecffeee
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-08-02 22:56:57 +0530

    footer defined

---
+++ src/pages/Footer.js
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return(
+     <div>
+        <h2>Contact Shivam:-</h2>
+        <p>Email:-</p>
+        <p>LinkedIn:- </p>
+        <p>GitHub</p>
+     </div>
+    )
+}
