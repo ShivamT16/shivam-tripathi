5750dbd30a62f6516606df199e3af759ed1629c0
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-08-05 23:53:42 +0530

    Intro defined

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './pages/NavBar';
+import { Intro } from './pages/Intro';
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <h1> Shivam Tripathi </h1>
       <Navbar />
+      <Intro />
 
       </Router>
     </div>
---
+++ src/pages/Intro.js
@@ -0,0 +1,11 @@
+export const Intro = () => {
+    return(
+        <div style={{display: "flex", backgroundColor: "black",justifyContent: "space-around"}}>
+            <div style={{textAlign: "left", color: "white"}}>
+                <h1 style={{fontSize: "5rem"}}>Shivam Tripathi</h1>
+                <h1>Frontend Developer</h1>
+            </div>
+            <img style={{height: "20rem", padding: "1rem", borderRadius: "2rem"}} src="https://as1.ftcdn.net/v2/jpg/02/12/88/82/1000_F_212888221_rZe9FeCt4sTmhGz35BeyehO6obT1bT9J.jpg" alt="" />
+        </div>
+    )
+}
