1dbfdc0e83e52215f43fde48968514f989f3cafb
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-09-05 12:59:07 +0530

    css: about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,70 +4,76 @@ import neogLogo from "../media/neoGlogo.png"
 import synexooLogo from "../media/synexoo_logo.png"
 import gbuLogo from "../media/gbuLogo.png"
 import { Link } from 'react-router-dom'
+import "./styles.css"
 
 export const About = () => {
   return (
-    <div style={{textAlign: "left"}}>
-    <h1>Experience</h1>
+    <div className='about-main'>
+    <h1 className='about-heading'>Experience</h1>
 
-    <div>
+    <div className='exp-card' >
      <div style={{display: "flex"}}>
-        <img style={{height: "5rem"}} src={synexooLogo} alt="comapny" />
-        <div>
-            <p>Frontend Development Intern</p>
+        <img className='card-img' src={synexooLogo} alt="comapny" />
+        <div className='card-detail'>
+            <h2>Frontend Development Intern</h2>
             <p>Synexoo, Noida</p>
+            <p>June 2024 – July 2024</p>
         </div>
      </div>
-     <p>June 2024 – July 2024</p>
-       <p>
-        <li>Optimized user authentication and search functionality for an e-commerce project</li>
+     
+       <p style={{lineHeight: "2rem"}} >
+        <li>Optimized user authentication and search functionality for an e-commerce project.</li>
         <li>Assisted in designing user interfaces for web and mobile applications, enhancing digital
-        products' visual appeal and functionality</li>
+        products' visual appeal and functionality.</li>
         <li>Collaborated with the design team to conduct user research, improving UI/UX design
-        experience by addressing key pain points</li>
+        experience by addressing key pain points.</li>
        </p>   
     </div>
 
-    <div>
+    <div className='exp-card1'>
      <div style={{display: "flex"}}>
-       <img style={{height: "5rem"}} src={neogLogo} alt="comapny" />
-       <div>
-        <p>Frontend (React) Skill Development Boot Camp</p>
+       <img className='card-img' src={neogLogo} alt="comapny" />
+       <div className='card-detail'>
+        <h2>Frontend (React) Skill Development Boot Camp</h2>
         <p>NeoG Camp </p>
+        <p>Oct 2023 – Apr 2024</p>
        </div>
      </div>
-     <p>Oct 2023 – Apr 2024</p>
+     
      <p>
        <li>Completed 5 <Link to="/projects" > projects</Link> in HTML, CSS, React, Redux, JavaScript, TypeScript</li>
      </p>   
     </div>
 
-    <div>
+    <div className='exp-card'>
         <div style={{display: "flex"}}>
-            <img style={{height: "5rem"}} src={wcsLogo} alt="comapny" />
-            <div>
-                <p>Technical Support Engineer</p>
+            <img className='card-img' src={wcsLogo} alt="comapny" />
+            <div className='card-detail'>
+                <h2>Technical Support Engineer</h2>
                 <p>WCS India Private Limited, Gurugram</p>
+                <p>Aug 2022 – Sept 2023</p>
             </div>
         </div>
-     <p>Aug 2022 – Sept 2023</p>
-      <p>
-       <li>Served as the primary point of contact for internal office employees regarding IT support</li>
-       <li>Provided technical support to clients regarding PBXs and telephony systems</li>
+     
+      <p style={{lineHeight: "2rem"}}>
+       <li>Served as the primary point of contact for internal office employees regarding IT support.</li>
+       <li>Provided technical support to clients regarding PBXs and telephony systems.</li>
       </p>   
     </div>
 
-    <h1>Education </h1>
+    <h1 className='about-heading'>Education </h1>
 
-    <div>
+    <div className='exp-card1'>
      <div style={{display: "flex"}}>
-       <img style={{height: "5rem"}} src={gbuLogo} alt="comapny" />
-       <div>
-        <p>Bachelor of Technology</p>
+       <img className='card-img' src={gbuLogo} alt="comapny" />
+       <div className='card-detail'>
+        <h3>Bachelor of Technology</h3>
         <p>Gautam Buddha University, Greater Noida </p>
+        <p>Aug 2018 – June 2022</p> 
+        <p> CGPA- 7.80 </p>
        </div>
      </div>
-     <p>Aug 2018 – June 2022</p> <p> CGPA- 7.80 </p> 
+      
     </div>
     
     </div>
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
+import { Navbar } from './NavBar';
 
 export const Error = () => {
     const error = useRouteError();
   return (
     <div>
+        <Navbar />
         <h1>Oopss!! </h1>
         <h2> Error {error.status} : {error.statusText} </h2>
         <h2>  {error.data}  </h2>
