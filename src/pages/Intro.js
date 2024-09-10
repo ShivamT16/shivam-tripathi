ce4f76d8f1b68ea7228b91364044bd95c8cdb9e7
Shivam Tripathi <109875608+ShivamT16@users.noreply.github.com>
2024-09-10 21:11:18 +0530

    css refactor

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,9 +1,10 @@
 import devLogo from "../media/dev-image.jpeg"
+import "./styles.css"
 
 export const Intro = () => {
     return(
-        <div style={{display: "flex",justifyContent: "space-around", margin: "0rem 3rem"}}>
-            <div style={{textAlign: "left"}}>
+        <div className="intro-main" >
+            <div className="intro-card">
                 <h1 style={{fontSize: "5rem"}}>Shivam Tripathi</h1>
                 <h1 style={{color: "blueviolet",fontSize: "3rem"}}>Frontend Developer</h1>
                 <p>Hi, I am Shivam Tripathi. I specialize in HTML, CSS, JavaScript, and ReactJS. I am passionate about creating scalable, high-performance web applications. I have developed dynamic projects such as e-commerce and social media websites to hone my skills. </p> 
